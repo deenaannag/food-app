08a40a3cf775663d263038d700ab44e48cc6515b
deenaannag <mca515@rajagiri.edu>
2021-05-22 14:32:51 +0530

    m

--- app/Http/Controllers/MessController.php
+++ app/Http/Controllers/MessController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\hash;
 use App\reg;
-use App\models\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Models\signup;
 use App\Models\addfood;
