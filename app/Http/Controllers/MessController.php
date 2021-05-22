14927e3c3653574e030381de65d867b8bef91b58
deenaannag <mca515@rajagiri.edu>
2021-05-22 14:34:51 +0530

    m

--- app/Http/Controllers/MessController.php
+++ app/Http/Controllers/MessController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\hash;
+use Illuminate\Support\Facades\Hash;
 use App\reg;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
